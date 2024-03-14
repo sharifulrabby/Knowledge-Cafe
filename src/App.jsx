500fd405e03c4b1e785dcc3ff5edb25077f09e7c
saifulsagor2 <saifulsagor963@gmail.com>
2024-03-14 12:39:31 +0600

    added the first step

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import Header from './Components/Header/Header'
+import Blogs from './Components/Blogs/Blogs'
+
+function App() {
+
+  return (
+    <>
+      <Header></Header>
+      <Blogs></Blogs>
+    </>
+  )
+}
+
+export default App
